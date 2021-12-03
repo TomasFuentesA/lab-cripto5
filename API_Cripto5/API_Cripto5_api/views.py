c470608301ddcd2f1f8fdfa0b8961e3a749903a1
TomasFuentesA <tomas.fuentes@mail.udp.cl>
2021-12-03 00:00:34 -0330

    ADD "NOne"

--- API_Cripto5/API_Cripto5_api/views.py
+++ API_Cripto5/API_Cripto5_api/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
-import json
 from django.http import HttpResponse
 from .models import Pdf
 from django.views.decorators.csrf import csrf_exempt
-import zlib
 # Create your views here.
 
 @csrf_exempt
@@ -13,24 +11,23 @@ def index(request):
     En esta parte del código se trato de descifrar lo que se encontraba encriptado
     y la idea era que una vez lograda esa parte, se unia con el if, que basicamente iba a separar
     cada dato y lo iba a ingresar
+    '''
     
-
+    
     #print(request)
     i = 0
     for key, value in request.POST.items():      
         print ("key",i,": ",key, "value",i,": ", value)
         i+=1
-    return HttpResponse("Hello, world!!")
   
-    '''
+
     if(request.method == 'POST'):
         
-        pdf_datos = Pdf(ip=request.POST.get('ip'), so=request.POST.get('so'), version=request.POST.get('version'), contrasenia=request.POST.get('contrasenia'))
+        pdf_datos = Pdf(ip="ip", so="so", version="version", contrasenia="contrasenia")
         Pdf.save(pdf_datos)
 
          
     datos = Pdf.objects.all()
     return render(request, 'pdfs.html', {'datos':datos})
-   
     
     
