11c2cb4cbc6ee844538e4ba6b87e73933fc3a133
TomasFuentesA <tomas.fuentes@mail.udp.cl>
2021-12-02 23:08:01 -0330

    1

--- API_Cripto5/API_Cripto5_api/views.py
+++ API_Cripto5/API_Cripto5_api/views.py
@@ -8,21 +8,29 @@ import zlib
 
 @csrf_exempt
 def index(request):
-    '''
     
+    '''
     En esta parte del código se trato de descifrar lo que se encontraba encriptado
     y la idea era que una vez lograda esa parte, se unia con el if, que basicamente iba a separar
     cada dato y lo iba a ingresar
+    
 
-    print(request.body)
+    #print(request)
     i = 0
     for key, value in request.POST.items():      
         print ("key",i,": ",key, "value",i,": ", value)
         i+=1
     return HttpResponse("Hello, world!!")
+  
     '''
     if(request.method == 'POST'):
+        
         pdf_datos = Pdf(ip=request.POST.get('ip'), so=request.POST.get('so'), version=request.POST.get('version'), contrasenia=request.POST.get('contrasenia'))
         Pdf.save(pdf_datos)
+
+         
     datos = Pdf.objects.all()
-    return render(request, 'pdf.html', {'datos':datos})
+    return render(request, 'pdfs.html', {'datos':datos})
+   
+    
+    
